70f1b9dab32ce4e058430bb58015250668027616
Evgeniy Pakhomov <nightmare23@yandex.ru>
2023-09-25 20:58:58 +0700

    Update hw_2_3.java
    Add description

--- hw_2_3.java
+++ hw_2_3.java
@@ -1,3 +1,6 @@
+//Если необходимо, исправьте данный код (задание 2 https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit)
+//Дан следующий код, исправьте его там, где требуется (задание 3 https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit)
+// Два задания в одном файле
 //https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit
 public class hw_2_3 {
     public static void main(String[] args) {
